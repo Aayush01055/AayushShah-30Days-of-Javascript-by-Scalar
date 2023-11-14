c6063579b393ee59c6e0a603b1e7ee001b381cb7
Aayush Shah <82150675+Aayush01055@users.noreply.github.com>
2023-11-14 10:17:15 +0000

    Day 6

---
+++ Day 6/Day6.js
@@ -0,0 +1,14 @@
+function sumofint(num1,num2)
+{
+    if(num1==num2)
+    {
+        return (num1+num2)*3;
+    }
+    else
+    {
+        return num1+num2;
+    }
+}
+
+console.log(sumofint(3,6));
+console.log(sumofint(5,5));
