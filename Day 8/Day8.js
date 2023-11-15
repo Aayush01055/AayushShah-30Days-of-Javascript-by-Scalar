2a3c9cfa4c52f378cafae643a111f48485459992
Aayush Shah <aayushshah342@gmail.com>
2023-11-15 10:08:50 +0530

    Day 8

--- Day 8/Day8.js
+++ Day 8/Day8.js
@@ -1,21 +1,20 @@
-function getSubsets(arr, length, start = 0, currentSubset = [], result = []) 
-{
-    if (currentSubset.length === length) 
-    {
-        result.push([...currentSubset]);
-        return;
+function getCombinations(array, subsetLength) {
+    const combinations = [];
+  
+    for (let i = 0; i < array.length; i++) {
+      for (let j = i + 1; j < array.length; j++) {
+        const subset = [array[i], array[j]];
+        if (subset.length === subsetLength) {
+          combinations.push(subset);
+        }
+      }
     }
+  
+    return combinations;
+  }
 
-    for (let i = start; i < arr.length; i++) 
-    {
-        currentSubset.push(arr[i]);
-        getSubsets(arr, length, i + 1, currentSubset, result);
-        currentSubset.pop();
-    }
+  const array = [1, 2, 3];
+const subsetLength = 2;
 
-    return result;
-}
-
-console.log(getSubsets([1, 2, 3], 2));
-console.log(getSubsets([1, 2], 2));
-console.log(getSubsets([1, 2, 3, 4], 2));
+const combinations = getCombinations(array, subsetLength);
+console.log(combinations);
